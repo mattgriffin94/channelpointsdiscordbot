0023ea8f836bc8ea1a8e28f10ecda9f2b771ce64
Matt Griffin <mattgriffin94@yahoo.com>
2022-12-23 17:23:57 -0800

    Login with Bot

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import dotenv from 'dotenv';
+import { Client, Events, GatewayIntentBits, IntentsBitField } from 'discord.js';
 
 dotenv.config();
-if (1 === 1) {
-	console.log(process.env.DISCORD_BOT_TOKEN);
+const token = process.env.DISCORD_BOT_TOKEN;
+
+// They really discourage using all intents, but I want to anyway
+const allIntents = new IntentsBitField();
+for (const intent in GatewayIntentBits) {
+	allIntents.add(GatewayIntentBits[intent as keyof typeof GatewayIntentBits]);
 }
+
+// Initialize Client
+const client = new Client({ intents: allIntents });
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+client.login(token);
