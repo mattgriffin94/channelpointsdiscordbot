d7ed7588dfc2e022185969fbcc775353acf64dda
Matt Griffin <mattgriffin94@yahoo.com>
2023-08-29 23:15:42 -0700

    some tests connecting to mongo

--- src/commands/tally.ts
+++ src/commands/tally.ts
@@ -1,12 +1,35 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 import { CommandWithInfo } from '../classes/CommandWithInfo';
+import { client } from '../db/MongoManager';
+import { ReturnDocument } from 'mongodb';
 
 function createTallyCommandWithInfo(): CommandWithInfo {
     const command = new SlashCommandBuilder().setName('tally').setDescription('Increases the tally')
         .addStringOption(option => option.setName('name').setDescription('The name of the tally to adjust').setRequired(true))
-        .addStringOption(option => option.setName('increment').setDescription('How much to adjust the tally.  Leave blank to read back current value').setRequired(false));
+        .addIntegerOption(option => option.setName('increment').setDescription('How much to adjust the tally.  Leave blank to read back current value').setRequired(false));
 
     async function execute(interaction: ChatInputCommandInteraction) {
+        try {
+
+            const name = interaction.options.getString('name');
+            var incrementBy = interaction.options.getInteger('increment');
+            if (incrementBy === null || incrementBy === undefined) {
+                incrementBy = 0;
+            }
+
+            const database = client.db('discord');
+            const collection = database.collection('keyValuePairs');
+    
+            const result = await collection.findOneAndUpdate(
+                { key: name },
+                { $inc: { value: incrementBy } },
+                { returnDocument: ReturnDocument.AFTER }
+            );
+
+            await interaction.reply(`hello from tally ${result}`);
+        } catch (error) {
+            console.error('Error incrementing number:', error);
+        }
         
         await interaction.reply("hello from tally");
     }
